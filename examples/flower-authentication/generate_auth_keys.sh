48ea7db88eafc8a6e5aa4a3522663379b52f6f0c
sarahhfalco <104618959+sarahhfalco@users.noreply.github.com>
2026-01-24 09:50:40 +0100

    Make auth example client count configurable

--- examples/flower-authentication/generate_auth_keys.sh
+++ examples/flower-authentication/generate_auth_keys.sh
@@ -11,19 +11,34 @@ mkdir -p $KEY_DIR
 
 rm -f $KEY_DIR/*
 
+CURVES=("secp112r1" "secp256k1" "sect571k1")
+SIZES=("112" "256" "512")
+
 generate_client_credentials() {
-    local num_clients=${1:-2} 
+    local num_clients=${1:-2}
     for ((i=1; i<=num_clients; i++))
     do
-        ssh-keygen -t ecdsa -b 384 -N "" -f "${KEY_DIR}/client_credentials_$i" -C ""
+        local curve_index=$(( (i - 1) % ${#CURVES[@]} ))
+        local curve_name=${CURVES[$curve_index]}
+        local curve_size=${SIZES[$curve_index]}
+
+        echo "[*] Generating client $i with curve ${curve_name} (${curve_size}-bit)"
+        openssl ecparam -name "$curve_name" -genkey -noout \
+            -out "${KEY_DIR}/client_credentials_${i}.key"
+        openssl ec -in "${KEY_DIR}/client_credentials_${i}.key" -pubout \
+            -out "${KEY_DIR}/client_credentials_${i}.pub"
     done
 }
 
 generate_client_credentials "$1"
 
-printf "%s" "$(cat "${KEY_DIR}/client_credentials_1.pub" | sed 's/.$//')" > $KEY_DIR/client_public_keys.csv
-for ((i=2; i<=${1:-2}; i++))
+for ((i=1; i<=${1:-2}; i++))
 do
-    printf ",%s" "$(sed 's/.$//' < "${KEY_DIR}/client_credentials_$i.pub")" >> $KEY_DIR/client_public_keys.csv
+    entry="$(openssl base64 -A -in "${KEY_DIR}/client_credentials_${i}.pub")"
+    if [ "$i" -eq 1 ]; then
+        printf "%s" "$entry" > $KEY_DIR/client_public_keys.csv
+    else
+        printf ",%s" "$entry" >> $KEY_DIR/client_public_keys.csv
+    fi
 done
 printf "\n" >> $KEY_DIR/client_public_keys.csv
