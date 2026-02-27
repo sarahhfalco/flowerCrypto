663a6faa1f716b56add3ade1aa8d31b17d814969
sarahfalco <sarah.falco.s@gmail.com>
2026-02-27 09:29:21 +0100

    push

--- examples/customCriptography/customCryptographyExample/task.py
+++ examples/customCriptography/customCryptographyExample/task.py
@@ -120,7 +120,7 @@ def get_model(model_name: str, num_classes=10, pretrained=True):
 
         return TinyCNN()
     elif model_name == "resnet18":
-        model = resnet18(pretrained=False)
+        model = resnet18(pretrained=True)
         # 🔧 adattamento per CIFAR (32x32)
         model.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
         model.maxpool = nn.Identity()  # rimuove maxpool iniziale
